c06489f91f3d78a2eef1933e9783abc40eeb62d0
ylizcano <113183144+ylizcano@users.noreply.github.com>
2022-09-13 01:29:59 -0500

    Update lanzador.pl

--- lanzador.pl
+++ lanzador.pl
@@ -12,7 +12,7 @@ Requerimientos:
     -La idea es iterar por ejecutables*cargas*repeticiones
 =cut
 
-#se crea el vector de ejecutables
+#se crea el vector de ejecutables.
 
 @Ejecutables = ("mm_main_int","mm_main_int_rnd","mm_main_double","mm_main_double_rnd");
 @Carga = ("300","500","700","900","1100","1200","1400","1600","1700","1800","1900","2000");
